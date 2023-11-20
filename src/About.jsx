e123e81c9a61dffd413f307038dcee5ec396df02
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-11-21 01:29:12 +0530

    commit

--- src/About.jsx
+++ src/About.jsx
@@ -26,19 +26,26 @@ export default function About(){
     <div className=' border p-6 bg-white bg-opacity-80 backdrop-filter rounded-lg shadow-lg font-semibold'>
             
 
-            <p>If you are logging in for first time :</p><br></br>
-            <li>Try any your mail as password and login.</li>
-            <li>You will recieve otp to your mail. Enter and submit. Kindly check in your spam mails.</li>
-            <li>Enter new password for future login and submit.</li>
-            <li>Select Team or Single.</li>
-            <li>Select a guide who have vacancies.</li>
-            <li>Enter project Details.</li>
-            <li>If you are doing project alone, Enter your details and submit.</li>
-            <li>If not, Enter team member 1 details first.</li>
-            <li>Enter second person details, click on verify.</li>
-            <li>Otp is sent the second team member mail should be entered and submit.</li>
-            <br></br><br></br>
-            
+        <p className="font-bold flex items-center justify-center text-xl">Welcome to the Guide Selection Portal for First-Year Students!</p><br></br>            
+        
+        <p className="font-semibold text-lg">This website is designed to help first-year students choose a guide who will support them throughout the academic year. Here's a step-by-step guide:</p>
+
+        <ol className=" pt-2 text-lg pl-4 list-decimal">
+            <li>Select a staff member as your guide. Each guide has 20 vacancies available.</li>
+            <li>After selecting your guide, provide the following information:</li>
+            <ul className="pt-2 pl-8 list-disc">
+                <li>Name</li>
+                <li>Registration number</li>
+                <li>Email</li>
+                <li>Phone number</li>
+            </ul>
+            <li>No duplicates are allowed, so ensure the information is unique.</li>
+            <li>Upload a passport-size photo, which must be less than 100KB in size.</li>
+            <li>After entering the details, submit your information.</li>
+        </ol>
+
+        <p className="flex pt-4 justify-center">That's all for the student registration process!</p>
+        <br></br><br></br>
 
           <div className=' flex justify-center'>
 
